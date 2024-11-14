b1174ed486a2788f944833dd67a0df3ff0dd0f26
JulienThevenoz <julien@thevenoz.ch>
2024-11-14 13:39:19 +0100

    created template MyThymio class

---
+++ MyThymio.py
@@ -0,0 +1,20 @@
+class MyThymio():
+    def __init__(self):
+        self.position = None
+        self.map = None
+        self.filter_module = None
+        self.local_navigation_module = None
+
+
+    def set_speed(self,speed):
+        # set motor1 to speed[0] and motor2 to speed[2]
+        pass
+
+
+    def get_sensor_reading(self,sensor_name):
+        pass
+
+
+    def avoid_obstacle(self):
+        #self.local_navigation_module.activate()
+        pass
