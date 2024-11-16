296b7dd817887e965c89f8e47393ba394c9eabd1
zhuoran-ren <zhuoran.ren@epfl.ch>
2024-11-16 12:47:52 +0100

    Update local_navigation.py

--- local_navigation.py
+++ local_navigation.py
@@ -1,4 +1,17 @@
+from tdmclient import ClientAsync, aw
+
 class local_navigation:
   def __init__(self):
-  def obstacle_avoidance(prox_horizontal, speed):
+    
+  def obstacle_avoidance(self, prox_horizontal, speed):
     return new_speed
+
+  def is_obstacle_clear(self, prox_horizontal):
+
+  def activate(self, prox_horizontal, speed):
+    while is not self.is_obstacle_clear(prox_horizontal):
+      speed = self.obstacle_avoidance(prox_horizontal, speed)
+    break
+      
+
+
