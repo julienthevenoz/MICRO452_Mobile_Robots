47b7122794676eb823a743670b2e9d1c36ac3003
zhuoran-ren <zhuoran.ren@epfl.ch>
2024-11-16 14:04:04 +0100

    Update global_navigation.py

--- global_navigation.py
+++ global_navigation.py
@@ -1,5 +1,11 @@
 class global_navigation:
+  '''
+  Could use some existing libraries: pyvisgraph
+  Input: start_point, end_point, obstacles(get from visual module)
+  Output: shortest path
+  '''
   def __init__(self):
 
   def shortest_path(self,start, goal):
+    
     return path
