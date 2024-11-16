553a042c4a5593d6068310c2d0c02e8b323b8b6e
zhuoran-ren <zhuoran.ren@epfl.ch>
2024-11-16 13:44:57 +0100

    Update local_navigation.py

--- local_navigation.py
+++ local_navigation.py
@@ -1,5 +1,3 @@
-from tdmclient import ClientAsync, aw
-
 class local_navigation:
   def __init__(self):
     self.threshold_high = None;
@@ -11,17 +9,6 @@ class local_navigation:
     Possible methods: ANN weights / Potential fields
     '''
     return new_speed
-
-  def is_obstacle_clear(self, prox_horizontal):
-    '''
-    Judging whether the obstacle is cleared or not
-    If there is any value in prox_horizontal larger than threshold_high, return Flase
-    If all values in prox_horizontal smaller than threshold_low, return True
-    '''
-    for sensor_value in prox_horizontal:
-      if sensor_value > self.threshold_high:
-        return False
-      if sensor_value < self.threshold_low:
         
   def activate(self, prox_horizontal, speed):
     while is not self.is_obstacle_clear(prox_horizontal):
