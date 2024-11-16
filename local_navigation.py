b70bce82de863cf162e38369b128a4134c0f24b3
zhuoran-ren <zhuoran.ren@epfl.ch>
2024-11-16 14:00:26 +0100

    Update local_navigation.py

--- local_navigation.py
+++ local_navigation.py
@@ -1,7 +1,6 @@
 class local_navigation:
   def __init__(self):
-    self.threshold_high = None;
-    self.threshold_low = None;
+    
     
   def obstacle_avoidance(self, prox_horizontal, speed):
     '''
@@ -9,11 +8,7 @@ class local_navigation:
     Possible methods: ANN weights / Potential fields
     '''
     return new_speed
-        
-  def activate(self, prox_horizontal, speed):
-    while is not self.is_obstacle_clear(prox_horizontal):
-      speed = self.obstacle_avoidance(prox_horizontal, speed)
-    break
+
       
 
 
