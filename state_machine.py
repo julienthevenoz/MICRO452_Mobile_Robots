93a6984b04406391751e0d014ce9b2cb9e375696
zhuoran-ren <zhuoran.ren@epfl.ch>
2024-11-16 13:54:34 +0100

    Update state_machine.py

--- state_machine.py
+++ state_machine.py
@@ -1,2 +1,14 @@
-class state_machine：
+class state_machine:
+  def __init__():
+    self.thymio = 
+    
+  def is_goal_reached(self):
+    
+  def is_obstacle_clear(self):
+
+  def state_uodate(self):
+
+  def run(self):
+  
+
 
