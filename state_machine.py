ddaf30a4e1426d822e1370c3844650a1a964f3e2
zhuoran-ren <zhuoran.ren@epfl.ch>
2024-11-16 13:59:23 +0100

    Update state_machine.py

--- state_machine.py
+++ state_machine.py
@@ -2,7 +2,8 @@ class state_machine:
   def __init__():
     self.thymio = MyThymio;
     self.state = state;
-    # 
+    
+    # parameters related to local navigation
     self.threshold_high = None;
     self.threshold_low = None;
     
@@ -13,6 +14,16 @@ class state_machine:
   def state_uodate(self):
 
   def run(self):
+    while is not self.is_goal_reached():
+      if state == 1:
+        # glabal_navigation
+        self.state_update()
+      if state == 2:
+        # local navigation
+        self.state_update()
+      if state == 3:
+        # reach the goal 
+        break
   
 
 
