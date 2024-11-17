db33be868d82cfad4f454f30124ec84115c48368
Tifaine <tifaine.mezencev@epfl.ch>
2024-11-17 10:18:38 +0100

    start fichier Vision - fonctions

---
+++ Vision.py
@@ -0,0 +1,22 @@
+# Camera & Image Processing
+
+def initialize_camera():
+    return
+
+def capture_image():
+    return
+
+# Map definition
+
+def detect_map_corners(image):
+
+# Object detection
+
+def detect_thymio_position():
+    return
+
+def detect_goal_position():
+    return
+
+def detect_obstacles(image):
+    return
