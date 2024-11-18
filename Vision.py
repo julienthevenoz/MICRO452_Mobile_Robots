198c48256f4fa474f68c29303cd657ef1f54dbf7
JulienThevenoz <julien@thevenoz.ch>
2024-11-18 18:45:03 +0100

    some progress

--- Vision.py
+++ Vision.py
@@ -39,13 +39,18 @@ def show_many_img(img_list, title_list):
 class Vision_module():
     def __init__(self, image=None):
         self.img = image
+        self.cam = None
 
     # Camera & Image Processing
 
-    def initialize_camera(self):
+    def initialize_camera(self, cam_port=4):  #on my PC, the webcam is in port 4
+        self.cam = cv2.VideoCapture(cam_port)
         return
 
     def capture_image(self):
+        result, image = self.cam.read()
+        print(result)
+        show_img(image, 'camera image')
         return
     
     # Map definition
@@ -58,11 +63,28 @@ class Vision_module():
     def extract_edge(self, img): ##### à merge/repmplacer par Image_correction #####
         # img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
         show_img(img,'supposedly masked image')
-        blurred_img = cv2.GaussianBlur(img, ksize=[5,5],sigmaY=10,sigmaX=10)  
-        show_img(blurred_img,"blurred img")
-        cannied_img = cv2.Canny(blurred_img,100,200)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        show_img(gray,"gray img")
+
+        # blurred_img = cv2.GaussianBlur(gray, ksize=[5,5],sigmaY=10,sigmaX=10)  
+        # show_img(blurred_img,"blurred img")
+
+        cannied_img = cv2.Canny(gray,75,200)
         show_img(cannied_img, "canny edge image")
-        return cannied_img
+
+
+        # Find contours
+        contours, hierarchy = cv2.findContours(cannied_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Draw contours   #this is just for testing
+        drawing = np.zeros((cannied_img.shape[0], cannied_img.shape[1], 3), dtype=np.uint8)
+        import random
+        for i in range(len(contours)):            
+            color = (random.randint(0,256), random.randint(0,256), random.randint(0,256))
+            cv2.drawContours(drawing, contours, i, color, 2, cv2.LINE_8, hierarchy, 0)
+        # Show in a window
+        show_img(drawing,'Contours')
+        return contours
 
     def get_colour_mask(self, img, lower, upper):
         original = img.copy()
@@ -71,6 +93,94 @@ class Vision_module():
         mask = cv2.inRange(img, lower, upper)
         show_img(mask,"mask")
         return mask
+    
+    def find_map_corners(self, contours):
+        #we sort our contours by area, in descending order, and we only need to keep first 5
+        # of them since we're looking for a very big object (the map)
+
+        cnts = sorted(contours, key=cv2.contourArea, reverse=True)[:5] 
+        found = False
+        #loop over the contours
+        for i,c in enumerate(cnts):
+            perimeter = cv2.arcLength(c,True)  #calculate length of the contour
+            approx = cv2.approxPolyDP(c, perimeter*0.02, True) #create a polygon that approximates the curve
+            drawing = np.zeros((self.img.shape[0], self.img.shape[1], 3), dtype=np.uint8)
+            drawn = cv2.drawContours(drawing,approx,-1, (0,255,0), 3)
+            # drawn = cv2.drawContours(drawing, c, -1, (0,0,255), 2)
+            print(len(approx))
+            show_img(drawn,f"contour nb{i}")
+            if len(approx) == 4:
+                map_contour = approx   #if the polygon has 4 corners, it's probably the rectangle of our map
+                found = True
+                break
+
+        if found:
+            print("Found !")
+            drawn = cv2.drawContours(self.img, map_contour,-1, (0,255,0), 2)
+            show_img(drawn,"map contours")
+            #map_contour is structed in a stupid way (4x1x2), so we do list comprehension
+            #magic to make it into a more logical 4x2 array
+            map_contour = np.array([xy for subarray in map_contour for xy in subarray])
+            return map_contour
+        else:
+            print("no 4 corner contour")
+            return None
+
+    def order_points(self,pts):
+        '''Takes an input vector of points (=size 4x2) representing 
+        the 4 corners of a rectangle and returns the same points re-ordered
+        as : Top-right, Top-left, Bottom-left, Bottom-right
+        '''
+        rect = np.zeros((4, 2), dtype = "float32")
+        # the top-left point will have the smallest sum, whereas
+        # the bottom-right point will have the largest sum
+        s = pts.sum(axis = 1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        # now, compute the difference between the points, the
+        # top-right point will have the smallest difference,
+        # whereas the bottom-left will have the largest difference
+        diff = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        # return the ordered coordinates
+        return rect
+    
+
+    def four_point_transform(self, image, pts):
+        # obtain a consistent order of the points and unpack them
+        # individually''
+        rect = self.order_points(pts)
+        (tl, tr, br, bl) = rect
+        # compute the width of the new image, which will be the
+        # maximum distance between bottom-right and bottom-left
+        # x-coordiates or the top-right and top-left x-coordinates
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+        # compute the height of the new image, which will be the
+        # maximum distance between the top-right and bottom-right
+        # y-coordinates or the top-left and bottom-left y-coordinates
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
+        # now that we have the dimensions of the new image, construct
+        # the set of destination points to obtain a "birds eye view",
+        # (i.e. top-down view) of the image, again specifying points
+        # in the top-left, top-right, bottom-right, and bottom-left
+        # order
+        dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype = "float32")
+        # compute the perspective transform matrix and then apply it
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+        # return the warped image
+        return warped
+
+
 
     def detect_thymio_position(image):
         return
@@ -85,14 +195,21 @@ class Vision_module():
         return 
 
 if __name__ == "__main__":
-    filename = 'Photos/Photo2.jpg'
+    filename = 'Photos/Photo4.jpg'
     img = cv2.imread(filename, cv2.IMREAD_COLOR)
     visio = Vision_module(img)
-    mask = visio.get_colour_mask(img, LOWER_GREEN, UPPER_GREEN)
-    masked_img = cv2.bitwise_and(img, img, mask=mask)  #apply color mask (keep only green pixels)
+    # mask = visio.get_colour_mask(img, LOWER_GREEN, UPPER_GREEN)
+    # masked_img = cv2.bitwise_and(img, img, mask=mask)  #apply color mask (keep only green pixels)
 
-    cannied_img = visio.extract_edge(masked_img)
+    contours = visio.extract_edge(img)
+    corners = visio.find_map_corners(contours)
+    #now that we have the 4 corners of the map, we have to order them
+    corners = visio.order_points(corners)
+    #now we can apply the transform to get a perpendicular top-view
+    top_view_img = visio.four_point_transform(img,corners)
+    show_img(top_view_img,'topi gang')
+
     
 
 
-# 
+
