fd74caa7366b2fca008295efa3a7cd51aca9e6f9
JulienThevenoz <julien@thevenoz.ch>
2024-11-28 12:59:08 +0100

    readded Order_points for consistent ordering. Don't know if it actually makes a difference in performance tho

--- Vision.py
+++ Vision.py
@@ -236,7 +236,7 @@ class Analysis:
             self.cam.release()
         cv2.destroyAllWindows()
 
-    def detect_aruco(sel, img):
+    def detect_aruco(self, img):
         '''Detect all aruco markers on the input image. Returns the array of the marker
         IDs and the array (nb_markers)x4x2 array of the 4 corners of each marker'''
         #convert to grayscale
@@ -247,8 +247,8 @@ class Analysis:
 
         markerCorners, markerIds, _ = detector.detectMarkers(gray)
         if markerIds is not None:
-            frame_markers = cv2.aruco.drawDetectedMarkers(img.copy(), markerCorners, markerIds)
             print(f"Detected markers: {markerIds.flatten()}")
+            markersCorners = [self.order_points(marker) for marker in markerCorners]
         else:
             print("no markers")
 
@@ -384,6 +384,27 @@ class Analysis:
             rescaled_y = self.map_size[1]*(rescaled_y - y_min) / (y_max - y_min)
             rescaled_pts.append((rescaled_x, rescaled_y))
         return rescaled_pts
+    
+    def order_points(self,pts):
+        '''Takes an input vector of points (=size 4x2) representing 
+        the 4 corners of a rectangle and returns the same points re-ordered
+        as : Top-right, Top-left, Bottom-left, Bottom-right
+        '''
+        pts = squeeze(pts)
+        rect = np.zeros((4, 2), dtype = "float32")
+        # the top-left point will have the smallest sum, whereas
+        # the bottom-right point will have the largest sum
+        s = pts.sum(axis = 1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        # now, compute the difference between the points, the
+        # top-right point will have the smallest difference,
+        # whereas the bottom-left will have the largest difference
+        diff = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        # return the ordered coordinates
+        return rect
         
     def highlight_corners(self, image, corners, title='highlighted corners', show=True):
         '''Draws a circle around the corners detected in this iteration,
