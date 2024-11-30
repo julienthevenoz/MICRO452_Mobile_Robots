c2f743fc1e9c0c82c510b28ebb35e9aaf74b7a11
ChristyElSkaff <christy.elskaff@epfl.ch>
2024-11-30 14:52:15 +0100

    combine local and global nav first try

--- motion_control.py
+++ motion_control.py
@@ -26,10 +26,10 @@ class MotionControl:
     self.threshold_low = 100
 
     # weights when implementing the local obstacle avoidance
-    self.obstSpeedGain = [6, 4, -2, -6, -8]
+    self.obstSpeedGain = [0.3, 0.2, -0.1, -0.3, -0.4]
 
     # parameters related to global navigation
-    self.goal_range = 2.0
+    self.goal_range = 10
 
   def is_obstacle(self, prox_horizontal):
     mark = 0
@@ -46,15 +46,18 @@ class MotionControl:
   def obstacle_avoidance(self):
     speed = self.get_motor_speed()
     prox_horizontal = self.read_prox_sensors()
-    if self.is_obstacle(prox_horizontal):
+    while self.is_obstacle(prox_horizontal):
+      delta = 0
       for i in range(5):
-        speed[0] = speed[0] + prox_horizontal[i] * self.obstSpeedGain[i]
-        speed[1] = speed[1] - prox_horizontal[i] * self.obstSpeedGain[i]
+        delta += prox_horizontal[i] * self.obstSpeedGain[i]
+      delta = max(-self.max_omega, min(delta, self.max_omega))
+      speed[0] = speed[0] + delta
+      speed[1] = speed[1] - delta
       speed[0] = int(speed[0])
       speed[1] = int(speed[1])
       self.thymio.set_motor_speed(speed[0], speed[1])
-    else:
-      self.thymio.set_motor_speed(30, 30)
+      speed = self.get_motor_speed()
+      prox_horizontal = self.read_prox_sensors()
 
   def move_to_next(self, current_pos, goal_pos, current_angle):
     '''
@@ -72,6 +75,7 @@ class MotionControl:
     return v, omega
 
   def path_tracking(self, robot_state, goal_point):
+    print('hi')
     if not robot_state or not goal_point:
       return False
     x, y, theta = robot_state
@@ -79,16 +83,18 @@ class MotionControl:
     delta_x = x_goal - x
     delta_y = y_goal - y
     distance_to_goal = np.sqrt(delta_x ** 2 + delta_y ** 2)
+    self.distance_to_goal = distance_to_goal
     if distance_to_goal < self.goal_range:
       print('reached goal')
       return True
     angle_to_goal = np.arctan2(delta_y, delta_x)
     angle_error = angle_to_goal - theta
-    v = self.Ka * distance_to_goal
+    # v = self.Ka * distance_to_goal
+    v = 80
     omega = self.Kb * angle_error
     v = max(-self.max_velocity, min(v, self.max_velocity))
     omega = max(-self.max_omega, min(omega, self.max_omega))
-    self.set_motor_speed(v-omega, v+omega)
+    self.set_motor_speed(v+omega, v-omega)
     return False
 
   def get_motor_speed(self):
